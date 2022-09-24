77d7610ae711e6c4054cd6abedc52d3fafc4d5ef
Coze Sebastien <sebastien_coze@ens.univ-artois.fr>
2022-09-24 23:34:37 +0200

    Ajout de la classe Contact permettant de représenter un contact.

--- src/Models/Contact.php
+++ src/Models/Contact.php
@@ -2,4 +2,24 @@
 
 namespace Php\ContactManager\Models;
 
-class Inf
+use JetBrains\PhpStorm\Internal\TentativeType;
+use ReturnTypeWillChange;
+
+/**
+ * Classe Contact correspondant à un contact.
+ */
+class Contact implements \JsonSerializable
+{
+
+    /**
+     * @var int $id Étant l'identifiant du contact.
+     */
+    protected int $id;
+
+    protected string $civilityTitle;
+
+    #[ReturnTypeWillChange] public function jsonSerialize()
+    {
+        // TODO: Implement jsonSerialize() method.
+    }
+}
