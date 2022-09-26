57cd1633634f2524060b4abcdbc073b51f6db540
Coze Sebastien <sebastien_coze@ens.univ-artois.fr>
2022-09-26 20:46:31 +0200

    Ajout de la vue home.blade.php

--- views/home.blade.php
+++ views/home.blade.php
@@ -1,1 +1,11 @@
-<?php
+@extends('master')
+
+@section('title', $title)
+
+@section('main')
+    <h2 id="#about">Bienvenue sur Contact Manager !</h2>
+    <p>
+        Contact Manager sert à gérer vos contacts. En effet, vous pouvez ajouter des contacts, les modifiers et les
+        supprimer.
+    </p>
+@endsection
